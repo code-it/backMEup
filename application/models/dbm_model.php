6852df18dbba867e459bd651decd8cc334801957
Mohanad Kaleia <mohanad@mohanad-laptop.(none)>
2013-04-19 15:00:54 +0300

    Adding dbm_model

--- application/models/dbm_model.php
+++ application/models/dbm_model.php
@@ -1,12 +1,12 @@
 <?php if (!defined("BASEPATH")) exit("No direct script access allowed");
     
 /**
- * Class name : Database_model
+ * Class name : DBM_model
  * 
  * Description :
  * This class contain functions to deal with the user database table (Add , Edit , Delete)
  * 
- * Created date ; 10-4-2013
+ * Created date ; 18-4-2013
  * Modification date : ---
  * Modfication reason : ---
  * Author : Mohanad Shab Kaleia
@@ -14,12 +14,12 @@
  */    
     
     
-class Database_model extends CI_Model
+class DBM_model extends CI_Model
 {
 	
 	//class variable
 	var $id;
-	var $database_name = "";		
+	var $dbm_name = "";		
 	
 	
 	/**
@@ -33,37 +33,37 @@ class Database_model extends CI_Model
 	
 	
 	/**
-	 * function name : addUDatabase
+	 * function name : addDBM
 	 * 
 	 * Description : 
-	 * add database for example backMeup 
+	 * add database manager
 	 * 
 	 * parameters:
-	 * dbm_id: the database manager id for example mysql id		
-	 * Created date ; 10-4-2013
+	 * 
+	 * Created date ; 18-4-2013
 	 * Modification date : ---
 	 * Modfication reason : ---
 	 * Author : Mohanad Shab Kaleia
 	 * contact : ms.kaleia@gmail.com
 	 */
-	 public function addDatabase($dbm_id)
+	 public function addDBM()
 	 {
-	 	$query = "insert into database 
-	 			  (dbm_id , database_name , created_date  ,  is_deleted)
+	 	$query = "insert into dbm 
+	 			  (name , created_date  ,  is_deleted)
 	 			  values
-	 			  ({$dbm_id} , '{$this->database_name}' , CURDATE() , 'F' ) 
+	 			  ('{$this->dbm_name}' , CURDATE() , 'F' ) 
 	 				";	
 		$this->db->query($query);
 	 }
 	 
 	 
 	 /**
-	 * function name : modifyDatabase
+	 * function name : modifyDBM
 	 * 
 	 * Description : 
 	 * edit database
 	 * 		
-	 * Created date ; 10-4-2013
+	 * Created date ; 18-4-2013
 	 * Modification date : ---
 	 * Modfication reason : ---
 	 * Author : Mohanad Shab Kaleia
@@ -71,8 +71,8 @@ class Database_model extends CI_Model
 	 */
 	 public function modifyDatabase()
 	 {
-	 	$query = "update database set
-	 			  database_name = '{$this->database_name}' , 	 			  	 			   
+	 	$query = "update dbm set
+	 			  name = '{$this->dbm_name}' , 	 			  	 			   
 	 			  modified_date = CURDATE()	 
 	 			  where id = {$this->id}";
 						  
@@ -82,20 +82,20 @@ class Database_model extends CI_Model
 	 
 	 
 	 /**
-	 * function name : deleteDatabase
+	 * function name : deleteDBM
 	 * 
 	 * Description : 
-	 * delete database  by make is_deleted field to be true
+	 * delete dbm  by make is_deleted field to be true
 	 * 		
-	 * Created date ; 10-4-2013
+	 * Created date ; 18-4-2013
 	 * Modification date : ---
 	 * Modfication reason : ---
 	 * Author : Mohanad Shab Kaleia
 	 * contact : ms.kaleia@gmail.com
 	 */
-	 public function deleteDatabase()
+	 public function deleteDBM()
 	 {	 	
-	 	$query = "update database set
+	 	$query = "update dbm set
 	 			  is_deleted = 'T'
 	 			  where id = {$this->id}";
 		$this->db->query($query);
@@ -105,20 +105,20 @@ class Database_model extends CI_Model
 	 
 	 
 	 /**
-	 * function name : getAllDatabaseInfo
+	 * function name : getAllDBM
 	 * 
 	 * Description : 
 	 * get all database record
 	 * 		
-	 * Created date ; 10-4-2013
+	 * Created date ; 18-4-2013
 	 * Modification date : ---
 	 * Modfication reason : ---
 	 * Author : Mohanad Shab Kaleia
 	 * contact : ms.kaleia@gmail.com
 	 */
-	 public function getAllDatabaseInfo()
+	 public function getAllDBM()
 	 {
-	 	$query = "select * from database where is_deleted='F'";	
+	 	$query = "select * from dbm where is_deleted='F'";	
 		$query = $this->db->query($query);
 		return $query->result_array();
 		//return $query->result(); 		
@@ -141,12 +141,12 @@ class Database_model extends CI_Model
 	 * Author : Mohanad Shab Kaleia
 	 * contact : ms.kaleia@gmail.com
 	 */
-	public function checkDatabaseIsExist()
+	public function checkDbmIsExist()
 	{		
-		$query = "select * from database where database_name='{$this->database_name}'";				
+		$query = "select * from dbm where name='{$this->dbm_name}'";				
 		$result = $this->db->query($query);
 		
-		// if there is a record of the same email address then return false				
+		// if there is a record of the same dbm name then return false				
 		if($result->num_rows() > 0)
 			return TRUE;
 		else 
@@ -157,45 +157,24 @@ class Database_model extends CI_Model
 	
 	
 	/**
-	 * function name : getById
+	 * function name : getDbmById
 	 * 
 	 * Description : 
 	 * get database by id
 	 * 		
-	 * Created date ; 10-4-2013
+	 * Created date ; 18-4-2013
 	 * Modification date : ---
 	 * Modfication reason : ---
 	 * Author : Mohanad Shab Kaleia
 	 * contact : ms.kaleia@gmail.com
 	 */
-	 public function getById($id)
+	 public function getDbmById($id)
 	 {
-	 	$query = "select * from database where id={$id}";	
+	 	$query = "select * from dbm where id={$id}";	
 		$query = $this->db->query($query);
 		return $query->result_array();			
 	 }	
-	 
-	 
-	 
-	 /**
-	 * function name : getDatabaseByDBM
-	 * 
-	 * Description : 
-	 * get database by dbm_id
-	 * paremeters:
-	  * dbm_id 		
-	 * Created date ; 10-4-2013
-	 * Modification date : ---
-	 * Modfication reason : ---
-	 * Author : Mohanad Shab Kaleia
-	 * contact : ms.kaleia@gmail.com
-	 */
-	 public function getDatabaseByDBM($dbm_id)
-	 {
-	 	$query = "select * from database where dbm_id={$dbm_id}";	
-		$query = $this->db->query($query);
-		return $query->result_array();			
-	 }
+	
 }    
    
 ?>
--- application/views/gen/header.php
+++ application/views/gen/header.php
@@ -7,7 +7,9 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="description" content="Database backup system" />
     <meta name="abstract" content="Database backup system" />
-    <meta name="keywords" content="backup , database , web based" />
+    <meta name="keywords" content="backup , database , web based" />   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 		
 	<link href="<?php echo base_url(); ?>favicon.ico" rel="shortcut icon" type="image/x-icon" />
 	
@@ -16,7 +18,8 @@
 	<!-- load css -->	
 	<link rel="stylesheet" media="all" type="text/css" href="<?php echo base_url();?>css/bootstrap.css" />
 	<link rel="stylesheet" media="all" type="text/css" href="<?php echo base_url();?>css/reset.css" />		    	
-	<link rel="stylesheet" media="all" type="text/css" href="<?php echo base_url();?>css/style.css" />	  
+	<link rel="stylesheet" media="all" type="text/css" href="<?php echo base_url();?>css/style.css" />
+	<link rel="stylesheet" media="all" type="text/css" href="<?php echo base_url();?>css/bootstrap-responsive.css" >	  
 </head>
 		
 <body>	
